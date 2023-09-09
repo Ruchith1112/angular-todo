03b6c2c64e07a3fd0cd0225e7058a7143bc2d35d
Ruchith1112 <112198225+Ruchith1112@users.noreply.github.com>
2023-09-09 19:40:46 +0530

    chnaged

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,36 +1,82 @@
-import { Component,OnInit } from '@angular/core';
+// import { Component,OnInit } from '@angular/core';
+// import { Todo } from '../../Todo';
+// @Component({
+//   selector: 'app-todos',
+//   templateUrl: './todos.component.html',
+//   styleUrls: ['./todos.component.css']
+// })
+// export class TodosComponent implements OnInit {
+// todos:Todo[]
+// localItem:string
+
+// constructor(){
+//   this.localItem=localStorage.getItem("todos")??""
+//   if(this.localItem==null){
+//   this.todos=[];
+//   }
+//   else{
+//     this.todos = JSON.parse(this.localItem);
+//   }
+// }
+
+// deleteTodo(todo:Todo){
+//   console.log(todo)
+//   const index=this.todos.indexOf(todo)
+//   this.todos.splice(index,1)
+//   localStorage.setItem("todos",JSON.stringify(this.todos));
+// }
+// addTodo(todo: Todo){
+//   console.log(todo)
+//   this.todos.push(todo)
+//   localStorage.setItem("todos",JSON.stringify(this.todos));
+// }
+// ngOnInit(): void {
+  
+// }
+// }
+
+
+import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../Todo';
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-todos:Todo[]
-localItem:string
+  todos: Todo[] = [];
+  localItem: string;
 
-constructor(){
-  this.localItem=localStorage.getItem("todos")??""
-  if(this.localItem==null){
-  this.todos=[];
-  }
-  else{
-    this.todos = JSON.parse(this.localItem);
-  }
-}
+  constructor() {
+    this.localItem = localStorage.getItem("todos") ?? "";
 
-deleteTodo(todo:Todo){
-  console.log(todo)
-  const index=this.todos.indexOf(todo)
-  this.todos.splice(index,1)
-  localStorage.setItem("todos",JSON.stringify(this.todos));
-}
-addTodo(todo: Todo){
-  console.log(todo)
-  this.todos.push(todo)
-  localStorage.setItem("todos",JSON.stringify(this.todos));
-}
-ngOnInit(): void {
-  
-}
+    if (this.localItem) {
+      try {
+        this.todos = JSON.parse(this.localItem);
+      } catch (e) {
+        console.error("Error parsing JSON from localStorage:", e);
+        // Handle the error gracefully, e.g., set this.todos to a default value.
+      }
+    }
+  }
+
+  deleteTodo(todo: Todo) {
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    }
+  }
+
+  addTodo(todo: Todo) {
+    console.log(todo);
+    this.todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+
+  ngOnInit(): void {
+    // Your initialization code here
+  }
 }
